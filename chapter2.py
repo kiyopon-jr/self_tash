3d6882a17759e09f33dbd51069852caa6f167cec
Dima <dreznuk23@gmail.com>
2024-09-11 15:00:24 +0300

    both chapters

---
+++ chapter2.py
@@ -0,0 +1,9 @@
+a = 7
+print(a)
+b = a
+print(b)
+prince = 99
+print(99)
+print(type(5))
+print(type(2.0))
+print(type(5*'2.0'))
---
+++ chapter3.py
@@ -0,0 +1,7 @@
+print(60*60)
+seconds_per_hour = 3600
+print(3600*24)
+second_per_day = 3600*24
+print(second_per_day/seconds_per_hour)
+print(second_per_day//seconds_per_hour)
+print((second_per_day/seconds_per_hour)==(second_per_day//seconds_per_hour))
